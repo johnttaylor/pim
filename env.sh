54e81027f1cdbeb3fe09306547364ff3f8a9e206
johnt <john.t.taylor@gmail.com>
2022-07-09 19:24:33 -0400

    update env.sh script to support specifing a path for the GCC toolchain

--- env.sh
+++ env.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 # This script sets up the PIM environment to use NQBP and the GCC compiler
-#
+# 
 # This script MUST BE 'sourced' since it sets environment variables
 #
+# usage: env.sh [<gcc-bin-path>|default]
+#
+# <gcc-bin-path>   Optional explicit path to the GCC bin/ directory. The 
+#                  default behavior is to use the GCC toolchain in the command
+#                  path.  If 'default' is specified than the environment
+#                  reverts to the native GCC toolchain
+#
+
 
 
 # Get the root directory
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Set optional GCC path
+if [ -n "${1}" ]; then
+    if [ ${1} = "default" ]; then
+        unset NQBP_GCC_BIN
+    else
+        export NQBP_GCC_BIN=${1}
+    fi
+fi
 
 # Put the current directory into the command path (simplifies invoking the nqbp.py scripts)
 export PATH=$PATH:./
@@ -19,8 +36,15 @@ export NQBP_BIN=$NQBP_XPKGS_ROOT/nqbp
 
 
 alias t="cd $HERE"
-alias bob="$HERE/xsrc/nqbp/other/bob.py"
-alias chuck="$HERE/xsrc/nqbp/other/chuck.py"
+alias bob="$NQBP_BIN/other/bob.py"
+alias chuck="$NQBP_BIN/other/chuck.py"
 
-# Assumes the GCC is already in the command path
-echo "Environment set (using native GCC compiler)"
+# Display which compiler is being used
+if [ -z "${NQBP_GCC_BIN}" ]; then
+    echo "Environment set (using native GCC compiler)"
+    gcc --version
+else
+    echo "Environment set (using GCC compiler @ ${NQBP_GCC_BIN})"
+    ${NQBP_GCC_BIN}/gcc --version
+fi
+
